c9fd158c3dcf2ca6aeda4c13b9bdba5ffa66d0b4
Shane Tomlinson <stomlinson@mozilla.com>
2014-05-20 11:11:31 +0100

    feature: Add an HttpProxyStrategy.

---
+++ test/lib/json-echo-server.js
@@ -0,0 +1,76 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+// A simple JSON message server. Listens for a single JSON request.
+
+var http = require('http');
+var Promise = require('bluebird');
+var util = require('util');
+
+function JsonEchoServer() {
+}
+
+JsonEchoServer.prototype = {
+  init: function (options, done) {
+    options = options || {};
+
+    var self = this;
+    return new Promise(function (resolve, reject) {
+      self.server = http.createServer(function (req, resp) {
+        var receivedData = '';
+
+        req.on('data', function (_data) {
+          receivedData += String(_data);
+        });
+
+        req.on('end', function () {
+          resp.statusCode = 200;
+          resp.end(receivedData);
+
+          self._getReceiveDeferred().fulfill(JSON.parse(receivedData));
+        });
+      });
+
+      self.server.listen(options.port || 0, function (err) {
+        if (err) return reject(err);
+
+        var port = self.server.address().port;
+        resolve(port);
+      });
+    });
+  },
+
+  close: function (done) {
+    this.server.close(done);
+  },
+
+  /**
+   * Listen for a request. Return a promise that is fulfilled when
+   * a request arrives.
+   *
+   * @params {boolean} shouldReset - if `true`, reset for next request.
+   *      Defaults to `false`.
+   */
+  listen: function (shouldReset) {
+    if (shouldReset) this.clear();
+
+    return this._getReceiveDeferred().promise;
+  },
+
+  /**
+   * Prepare for the next request. Clears any outstanding promises.
+   */
+  clear: function () {
+    this.receiveDeferred = null;
+  },
+
+  _getReceiveDeferred: function () {
+    if (! this.receiveDeferred) this.receiveDeferred = Promise.defer();
+    return this.receiveDeferred;
+  }
+};
+
+module.exports = JsonEchoServer;
--- test/spec/proxy-strategies/http_test.js
+++ test/spec/proxy-strategies/http_test.js
@@ -2,68 +2,88 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var http = require('http');
 var assert = require('chai').assert;
+var Promise = require('bluebird');
 
+var JsonEchoServer = require('../../lib/json-echo-server');
 var HttpProxy = require('../../../lib/proxy-strategies/http');
 
 describe('lib/proxy-strategies/http', function () {
-  var httpProxy, server, receivedData;
+  var httpProxy, server, receivePromise;
 
-  beforeEach(function (done) {
-    receivedData = null;
+  before(function () {
+    server = new JsonEchoServer();
+    return server.init({})
+        .then(function (port) {
+           httpProxy = new HttpProxy();
+           httpProxy.init({
+             loadEventUrl: 'http://127.0.0.1:' + port + '/metrics/load',
+             unloadEventUrl: 'http://127.0.0.1:' + port + '/metrics/unload',
+             maxCacheSize: 3
+           });
 
-    server = http.createServer(function (req, resp) {
-      receivedData = true;
-      console.error('received some data!');
-
-      resp.statusCode = 200;
-      resp.end('ok');
-    });
-
-    server.listen(0, function (err) {
-      var port = server.address().port;
-      console.error('listening on port: %s', port);
-
-      httpProxy = new HttpProxy();
-      httpProxy.init({
-        loadEventUrl: 'http://127.0.0.1:' + port + '/metrics/load',
-        unloadEventUrl: 'http://127.0.0.1:' + port + '/metrics/unload',
-        maxCacheSize: 3
-      });
-
-      done();
-    });
+        });
   });
 
-  afterEach(function (done) {
+  beforeEach(function () {
+    // reset the request promise between each test.
+    receivePromise = server.listen(true);
+  });
+
+  after(function (done) {
     httpProxy.destroy();
     httpProxy = null;
     server.close(done);
   });
 
   describe('storeLoadEvent', function () {
-    it('sends events to the collector after maxCacheSize is reached', function (done) {
-      httpProxy.storeLoadEvent({ uuid: 'event1' })
-        .then(function () {
-          return httpProxy.storeLoadEvent({ uuid: 'event2' });
-        })
-        .then(function () {
-          return httpProxy.storeLoadEvent({ uuid: 'event3' });
-        })
-        .then(function () {
-          setTimeout(function () {
-            assert.ok(receivedData);
-            done();
-          }, 1000);
-        });
+    it('sends events to the collector after maxCacheSize is reached', function () {
+      httpProxy.storeLoadEvent({ uuid: 'loadEvent1' });
+      httpProxy.storeLoadEvent({ uuid: 'loadEvent2' });
+      httpProxy.storeLoadEvent({ uuid: 'loadEvent3' });
+
+      return receivePromise
+          .then(function (data) {
+            assert.deepEqual(data, [
+              { uuid: 'loadEvent1' },
+              { uuid: 'loadEvent2' },
+              { uuid: 'loadEvent3' }
+            ]);
+          });
     });
   });
 
   describe('storeUnloadEvent', function () {
+    it('sends events to the collector after maxCacheSize is reached', function () {
+      httpProxy.storeUnloadEvent({ uuid: 'unloadEvent1' })
+      httpProxy.storeUnloadEvent({ uuid: 'unloadEvent2' });
+      httpProxy.storeUnloadEvent({ uuid: 'unloadEvent3' });
+
+      return receivePromise
+          .then(function (data) {
+            assert.deepEqual(data, [
+              { uuid: 'unloadEvent1' },
+              { uuid: 'unloadEvent2' },
+              { uuid: 'unloadEvent3' }
+            ]);
+          });
+    });
   });
 
   describe('flush', function () {
+    it('sends events to the collector even if maxCacheSize is not reached', function () {
+      httpProxy.storeUnloadEvent({ uuid: 'flushEvent1' })
+      httpProxy.storeUnloadEvent({ uuid: 'flushEvent2' });
+      httpProxy.flush();
+
+      return receivePromise
+          .then(function (data) {
+            assert.deepEqual(data, [
+              { uuid: 'flushEvent1' },
+              { uuid: 'flushEvent2' }
+            ]);
+          });
+    });
   });
 });
 
