10862ec2f6ff8d68853982a911dbecf1bd574037
Shane Tomlinson <stomlinson@mozilla.com>
2014-05-24 14:17:57 +0100

    feat: Expose middleware, Composite, collectors from the index file.
    fixes #4
    fixes #14

---
+++ test/spec/index_test.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+var assert = require('chai').assert;
+
+var index = require('../../index');
+
+describe('index', function () {
+  it('should expose middleware, Composite, collectors', function () {
+    assert.ok(index.middleware);
+    assert.ok(index.Composite);
+    assert.ok(index.collectors);
+  })
+});
+
